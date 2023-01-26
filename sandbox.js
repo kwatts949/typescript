723217209480a73f61ca4e52981c55fb8418bb7b
Kayleigh Watts <kwatts949@gmail.com>
2023-01-26 12:34:18 +0000

    Create JS and TS files

---
+++ sandbox.js
@@ -0,0 +1,5 @@
+// To convert TS to JS
+// Terminal: tsc sandbox.ts sandbox.js or where files share same name: tsc sandbox.ts
+
+var character = "Kay";
+console.log(character);
---
+++ sandbox.ts
@@ -0,0 +1,5 @@
+// Browsers do not understand TS so .ts files have to be compiled into JS.
+
+const character = "Kay"
+
+console.log(character)
